4576246c77bcd3f0b8fc366c57855ab9f22a8b9c
Tushar Sharma <tushar.sharma@cubexo.com>
2023-02-16 20:10:23 +0530

    second push

--- src/components/requestLeave.js
+++ src/components/requestLeave.js
@@ -1,15 +1,70 @@
-import { Box, FormControl, TextField, Typography } from "@mui/material";
+import { Box, Button, FormControl, TextField, Typography } from "@mui/material";
+import { textAlign } from "@mui/system";
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker'
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
 
 
 function ApplayForLeave() {
-    const [value, setValue] = useState([null, null]);
+    const [value, setValue] = useState([null, null])
+    const [name, setName] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState();
+    const [comments, setComments] = useState("");
 
-    return (
+    const employee_data = {
+
+        name: name,
+        start_date: startDate,
+        end_date: endDate,
+        comments: comments
+    }
+
+    const leaveData = async () => {
+        await axios.post("http://127.0.0.1:8000/api/vacation/", employee_data)
+            .then((res) => {
+                console.log(res)
+            toast.success("Successfully submited")
+
+            })
+            .catch((err) => {
+                console.log(err)
+                toast.warn(" All Filds are required")
+
+            })
+        clearFields()
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        leaveData()
+        
+    }
+    const clearFields = (e) => {
+        setValue([null, null])
+        setName("")
+        setStartDate("")
+        setEndDate("")
+        setComments("")
+    }
+
+    useEffect(() => {
+        if (value[1]) {
+            const start = new Date(value[0].$d)
+            const ens = new Date(value[1].$d)
+            setStartDate(`${start.getFullYear()}-${start?.getMonth() + 1}-${start?.getDate()}`)
+            setEndDate(`${ens.getFullYear()}-${ens?.getMonth() + 1}-${ens?.getDate()}`)
+        }
+    }, [value])
+
+
+    return ( 
         <>
             <div className="man-box">
                 <div className="text-label">
@@ -18,10 +73,14 @@ function ApplayForLeave() {
                 <div className="form-box">
                     <FormControl>
                         <div className="text-input">
-                            <TextField fullWidth id="outlined-basic" label="Enter your name" variant="outlined" />
+                            <TextField fullWidth
+                                id="outlined-basic"
+                                label="Enter your name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)} variant="outlined" />
                         </div>
                         <div className="date-input">
-                            <Typography sx={{ mt: 2, mb: 3 }}>Choose Date</Typography>
+                            <Typography style={{ textAlign: "left" }} sx={{ mt: 2, mb: 3 }}>Choose Date</Typography>
                             <LocalizationProvider
                                 dateAdapter={AdapterDayjs}
                                 localeText={{ start: 'Check-in', end: 'Check-out' }}
@@ -29,7 +88,7 @@ function ApplayForLeave() {
                                 <DateRangePicker
                                     value={value}
                                     onChange={(newValue) => {
-                                        setValue(newValue);
+                                        setValue(newValue)
                                     }}
                                     renderInput={(startProps, endProps) => (
                                         <>
@@ -46,10 +105,28 @@ function ApplayForLeave() {
                             id="outlined-multiline-static"
                             label="Comments"
                             multiline
+                            value={comments}
                             rows={4}
                             defaultValue="Reason"
+                            onChange={(e) => setComments(e.target.value)}
                         />
-                        {/* Reason */}
+                        <div className="submit-btn">
+                            <Button onClick={handleSubmit} style={{ width: "100px" }} variant="contained">Submit</Button>
+                            <ToastContainer
+                                position="top-right"
+                                autoClose={5000}
+                                hideProgressBar={false}
+                                newestOnTop={false}
+                                closeOnClick
+                                rtl={false}
+                                pauseOnFocusLoss
+                                draggable
+                                pauseOnHover
+                                theme="light"
+                            />
+                            {/* Same as */}
+                            <ToastContainer />
+                        </div>
                     </FormControl>
                 </div>
             </div>
--- src/components/viewLeaveRequest.js
+++ src/components/viewLeaveRequest.js
@@ -1,24 +1,61 @@
-import * as React from 'react';
+import { useEffect, useState } from "react";
+
 import dayjs from 'dayjs';
 import { Calendar, dayjsLocalizer } from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import axios from 'axios';
 
 const localizer = dayjsLocalizer(dayjs)
 
 export default function ViewLeaveCalender() {
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    const [name, setName] = useState("");
+    const [comments, setComments] = useState("");
 
+
+    const leaveRecord = [{
+        title: comments,
+        start: start,
+        end: end,
+        // allDay: true,
+    }]
+
+    const viewLeaveData = async () => {
+        await axios.get("http://127.0.0.1:8000/api/vacation/")
+            .then((res) => {
+                // setName(res.)
+                console.log(res?.data)
+                res?.data.map((rec) => {
+                    console.log(rec?.name);
+                    setName(rec?.name);
+                    setStart(rec?.start_date);
+                    setEnd(rec?.end_date);
+                    setComments(rec?.comments);
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    useEffect(() => {
+        viewLeaveData()
+    }, [])
+
+    let changeFormate = name.toUpperCase() 
 
     return (
         <div>
             <div className="cal-box">
+            <span className="text-box">{changeFormate}</span>
+
                 <Calendar
                     localizer={localizer}
-                    // events={myEventsList}
+                    events={leaveRecord}
                     startAccessor="start"
                     endAccessor="end"
-                    style={{ height: 500 }}
-                    
+                 
+                    style={{ height: 500, marginTop: 20 }}
                 />
             </div>
 
